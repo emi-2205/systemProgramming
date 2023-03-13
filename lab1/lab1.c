a10aff2a4c374e9c9212c5cb6a2cc3a088211ed3
EMILIO JOSEPH GRIECO <s317869@labinf88.polito.it>
2023-03-13 15:50:28 +0100

    lab1

---
+++ lab1/lab1.c
@@ -0,0 +1,11 @@
+//
+// Created by s317869 on 13/03/23.
+//
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    printf("Usage: %s, %s, %s<filename>\n", argv[0], argv[1], argv[2]);
+    
+    return 1;
+}
